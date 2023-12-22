20a72443249c538224cf9ae7b04553c8019eac37
n1lkas <144939602+n1lkas@users.noreply.github.com>
2023-12-22 19:50:51 +0300

    Add files via upload

---
+++ Source1.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include "1.cpp"
+#include "2.cpp"
+#include "3.cpp"
+#include "4.cpp"
+
+using namespace std;
+
+int main()
+{
+    setlocale(0, "");
+    system("cls");
+    int lab;
+    cout << "Выберите номер лабораторной работы:";
+   
+    cin >> lab;
+    system("cls");
+    switch (lab)
+    {
+    case 1:
+        main1();
+        main();
+        break;
+    case 2:
+        main2();
+        main();
+        break;
+    case 3:
+        main3();
+        main();
+        break;
+    case 4:
+    main4();
+    main();
+        break;
+    case 0:
+        exit(1);
+    default:
+        main();
+        break;
+    }
+    return 0;
+}
